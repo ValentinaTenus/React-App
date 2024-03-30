4578db8aeb7104e710a0e587b379bbd90cd8810c
ValentinaTenus <120641395+ValentinaTenus@users.noreply.github.com>
2024-03-30 13:54:44 +0100

    + export types

---
+++ frontend/src/common/types/types.ts
@@ -0,0 +1,7 @@
+export { type ListOption } from './list-options';
+export {
+    type IConfig,
+    type ServerErrorDetail,
+    type ServerErrorResponse,
+    type ValueOf 
+} from 'shared/build/index.js';
