ac250346345cb45d92aa0fa67ee1165d5da5d814
ValentinaTenus <120641395+ValentinaTenus@users.noreply.github.com>
2024-03-30 11:37:08 +0100

    + enums

---
+++ frontend/src/common/enums/enums.ts
@@ -0,0 +1,4 @@
+export { IconName } from './icon-name.enum';
+export { IconSize } from './icon-size.enum';
+export { DataStatus } from './data-status.enum';
+export { ApiPath, AppEnvironment, ContentType, ServerErrorType } from 'shared/build/index.js';
