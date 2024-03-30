88de68d9cf4faaf6c1d5fb298a4e66374d0fe2e1
ValentinaTenus <120641395+ValentinaTenus@users.noreply.github.com>
2024-03-30 11:00:45 +0100

    + app module

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,9 +1,33 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { KnexModule } from 'nest-knexjs';
+import { CardsModule } from './cards/cards.module';
+import { ListsModule } from './lists/lists.module';
+import { ActivityModule } from './activity/activity.module';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot(),
+    KnexModule.forRoot({
+      config: {
+        client: 'postgres',
+        version: '15.2',
+        useNullAsDefault: true,
+        connection: {
+          host: 'localhost',
+          port: 9098,
+          user: 'postgres',
+          password: '1994',
+          database: 'tasks',
+        },
+      },
+    }),
+    CardsModule,
+    ListsModule,
+    ActivityModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
